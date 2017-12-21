64cde56e33da1f792bb2327f6679e16b1039b0ea
talshadar <mattbryan@gmail.com>
2017-12-20 19:42:27 -0700

    tweaking php7 issues

--- index.php
+++ index.php
@@ -8,6 +8,7 @@
 ##########################################################################
 // Report all PHP errors (see changelog)
 error_reporting(E_ALL);
+ini_set("display_errors", 1);
 
 session_start();
 /*

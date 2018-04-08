d619a8fb8869678efd51b57c44297d75072f00d1
Matt <mattbryan@gmail.com>
2018-04-08 12:42:23 -0500

    Fixed Admin access

--- admin.php
+++ admin.php
@@ -12,7 +12,16 @@ ini_set("display_errors", 1);
 //ini_set('memory_limit', '-1');
 session_start();
 
-if (($_SESSION['access'] != "granted"))
+if (array_key_exists('access', $_SESSION))
+{
+  if (($_SESSION['access'] != "granted"))
+  {
+    header("Location: login.php");
+    exit;
+  }
+
+}
+else
 {
     header("Location: login.php");
     exit;
